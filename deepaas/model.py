5ef2fbae2fff2119e390c4041da74f309562de5f
ignacio <ignacio.heredia.cacha@gmail.com>
2019-02-25 13:51:45 +0100

    add test_args functionality

--- deepaas/model.py
+++ deepaas/model.py
@@ -109,6 +109,11 @@ class BaseModel(object):
         """TBD."""
         raise NotImplementedError()
 
+    @abc.abstractmethod
+    def get_test_args(self, *args):
+        """TBD."""
+        raise NotImplementedError()
+
 
 class TestModel(BaseModel):
     """Dummy model implementing minimal functionality.
@@ -131,6 +136,9 @@ class TestModel(BaseModel):
         return super(TestModel, self).train(*args)
 
     def get_train_args(self, *args):
+        return {}
+
+    def get_test_args(self, *args):
         return {}
 
     def get_metadata(self):
@@ -209,5 +217,12 @@ class ModelWrapper(object):
     def get_train_args(self, *args):
         try:
             return self._get_method("get_train_args")(*args)
+        except exceptions.NotImplemented:
+            return {}
+
+    @catch_error
+    def get_test_args(self, *args):
+        try:
+            return self._get_method("get_test_args")(*args)
         except exceptions.NotImplemented:
             return {}
