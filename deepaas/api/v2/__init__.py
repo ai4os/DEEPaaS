b4dc3db4b1793e6774f04777935231acf87080a0
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2024-09-30 00:45:03 +0200

    fix: comment training responses

--- deepaas/api/v2/__init__.py
+++ deepaas/api/v2/__init__.py
@@ -22,6 +22,7 @@ from deepaas.api.v2 import debug as v2_debug
 from deepaas.api.v2 import models as v2_model
 from deepaas.api.v2 import predict as v2_predict
 from deepaas.api.v2 import responses
+
 # from deepaas.api.v2 import train as v2_train
 from deepaas import log
 
@@ -56,7 +57,7 @@ def get_app(enable_train=True, enable_predict=True):
         get_v2_version,
         methods=["GET"],
         tags=["version"],
-        response_model=responses.Versions
+        response_model=responses.Versions,
     )
 
     return APP
