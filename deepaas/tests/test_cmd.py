a7b27a795fd6b82066bfd54a124fa4a213ecf90f
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2025-03-21 12:16:33 +0100

    fix: improve unit testing

--- deepaas/tests/test_cmd.py
+++ deepaas/tests/test_cmd.py
@@ -14,9 +14,10 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import pathlib
 import sys
+from unittest import mock
 
-import mock
 from oslo_config import cfg
 import pytest
 
@@ -26,52 +27,144 @@ import deepaas.config
 CONF = cfg.CONF
 
 
-@mock.patch("deepaas.cmd._shutdown.handle_signals")
-@mock.patch("aiohttp.web.run_app")
-@mock.patch("deepaas.api.get_app")
-async def test_run(m_get_app, m_run_app, m_handle_signals):
-    m = mock.MagicMock()
-    m_get_app.return_value = m
-    with mock.patch.object(sys, "argv", ["deepaas-run"]):
-        run.main()
-    m_get_app.assert_called_once()
-    m_run_app.assert_called_with(
-        mock.ANY,
+@pytest.fixture
+def fastapi_app_fixture():
+    return mock.MagicMock()
+
+
+@pytest.fixture
+def uvicorn_run_fixture():
+    return mock.MagicMock()
+
+
+async def test_run(monkeypatch, fastapi_app_fixture, uvicorn_run_fixture):
+    """Test the run command."""
+    # uvicorn_run_fixture = mock.MagicMock()
+    monkeypatch.setattr(deepaas.cmd._shutdown, "handle_signals", mock.MagicMock())
+    monkeypatch.setattr(deepaas.api, "get_fastapi_app", fastapi_app_fixture)
+    monkeypatch.setattr("uvicorn.run", uvicorn_run_fixture)
+
+    # Monkeypatch the argv, as otherwise it will get the testing cmd line
+    monkeypatch.setattr(sys, "argv", ["deepaas-run"])
+    run.main()
+
+    fastapi_app_fixture.assert_called_once()
+    uvicorn_run_fixture.assert_called_with(
+        fastapi_app_fixture(),
         host="127.0.0.1",
         port=5000,
     )
-    m_handle_signals.assert_called_once()
+
+
+# @mock.patch("deepaas.cmd._shutdown.handle_signals")
+# @mock.patch("aiohttp.web.run_app")
+# @mock.patch("deepaas.api.get_app")
+# async def test_run(m_get_app, m_run_app, m_handle_signals):
+#     m = mock.MagicMock()
+#     m_get_app.return_value = m
+#     with mock.patch.object(sys, "argv", ["deepaas-run"]):
+#         run.main()
+#     m_get_app.assert_called_once()
+#     m_run_app.assert_called_with(
+#         mock.ANY,
+#         host="127.0.0.1",
+#         port=5000,
+#     )
+#     m_handle_signals.assert_called_once()
 
 
 @pytest.fixture
 def cfg_fixture():
+    # Keep track of which flags were overridden
+    overridden_flags = []
+
     def set_flag(flag, value):
         CONF.set_override(flag, value)
+        overridden_flags.append(flag)
 
-    return set_flag
+    yield set_flag
+
+    # Clean up after the test
+    for flag in overridden_flags:
+        CONF.clear_override(flag)
 
 
-@mock.patch("deepaas.cmd._shutdown.handle_signals")
-@mock.patch("aiohttp.web.run_app")
-@mock.patch("deepaas.api.get_app")
 async def test_run_custom_ip_port(
-    m_get_app, m_run_app, m_handle_signals, cfg_fixture, monkeypatch
+    monkeypatch, cfg_fixture, fastapi_app_fixture, uvicorn_run_fixture
 ):
-
+    """Run the cmd line with a custom IP and port."""
     monkeypatch.setattr(deepaas.config, "setup", lambda x: None)
-    m = mock.MagicMock()
-    m_get_app.return_value = m
+
     ip = "1.1.1.1"
     port = 1234
-
     cfg_fixture("listen_ip", ip)
     cfg_fixture("listen_port", port)
-    with mock.patch.object(sys, "argv", ["deepaas-run"]):
-        run.main()
-    m_get_app.assert_called_once()
-    m_run_app.assert_called_with(
-        mock.ANY,
-        host=ip,
-        port=port,
+
+    monkeypatch.setattr(deepaas.api, "get_fastapi_app", fastapi_app_fixture)
+    monkeypatch.setattr("uvicorn.run", uvicorn_run_fixture)
+
+    monkeypatch.setattr(sys, "argv", ["deepaas-run"])
+    run.main()
+
+    fastapi_app_fixture.assert_called_once()
+    uvicorn_run_fixture.assert_called_with(fastapi_app_fixture(), host=ip, port=port)
+
+
+async def test_custom_base_path(monkeypatch, cfg_fixture, fastapi_app_fixture):
+    """Run the cmd line with a custom base path."""
+    monkeypatch.setattr(deepaas.config, "setup", lambda x: None)
+
+    base_path = "/foo/bar"
+    cfg_fixture("base_path", base_path)
+
+    monkeypatch.setattr(deepaas.api, "get_fastapi_app", fastapi_app_fixture)
+    monkeypatch.setattr("uvicorn.run", mock.MagicMock())
+
+    monkeypatch.setattr(sys, "argv", ["deepaas-run"])
+    run.main()
+
+    fastapi_app_fixture.assert_called_once()
+    fastapi_app_fixture.assert_called_with(
+        enable_doc=True, enable_predict=True, base_path=str(pathlib.Path(base_path))
     )
-    m_handle_signals.assert_called_once()
+
+
+async def test_custom_base_path_no_slash(monkeypatch, cfg_fixture, fastapi_app_fixture):
+    """Run the cmd line with a custom base path without a slash."""
+    monkeypatch.setattr(deepaas.config, "setup", lambda x: None)
+
+    base_path = "foo/bar"
+    cfg_fixture("base_path", base_path)
+
+    monkeypatch.setattr(deepaas.api, "get_fastapi_app", fastapi_app_fixture)
+    monkeypatch.setattr("uvicorn.run", mock.MagicMock())
+
+    monkeypatch.setattr(sys, "argv", ["deepaas-run"])
+    mock_exit = mock.MagicMock()
+    monkeypatch.setattr(sys, "exit", mock_exit)
+    run.main()
+
+    mock_exit.assert_called_once_with(1)
+    fastapi_app_fixture.assert_called_once()
+    fastapi_app_fixture.assert_called_with(
+        enable_doc=True, enable_predict=True, base_path=str(pathlib.Path(base_path))
+    )
+
+
+async def test_predict_and_doc_endpoints(monkeypatch, cfg_fixture, fastapi_app_fixture):
+    """Run the cmd line with predict and doc endpoints disabled."""
+    monkeypatch.setattr(deepaas.config, "setup", lambda x: None)
+
+    cfg_fixture("doc_endpoint", False)
+    cfg_fixture("predict_endpoint", False)
+
+    monkeypatch.setattr(deepaas.api, "get_fastapi_app", fastapi_app_fixture)
+    monkeypatch.setattr("uvicorn.run", mock.MagicMock())
+
+    monkeypatch.setattr(sys, "argv", ["deepaas-run"])
+    run.main()
+
+    fastapi_app_fixture.assert_called_once()
+    fastapi_app_fixture.assert_called_with(
+        enable_doc=False, enable_predict=False, base_path=""
+    )
